cbed677743210ff77ba3774a152f98e33287ba8e
kbeliauski <bkotutby1112@icloud.com>
2020-04-14 09:36:47 -0400

    Initial Commit

---
+++ ChecklistApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChecklistApp
+//
+//  Created by Kiryl Beliauski on 4/14/20.
+//  Copyright © 2020 Kiryl Beliauski. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
