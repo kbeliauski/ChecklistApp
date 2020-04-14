082c8681397d46397d274b8342dd7ecf15e70b9f
kbeliauski <bkotutby1112@icloud.com>
2020-04-14 16:06:39 -0400

    added arrays and sections of the list

--- ChecklistApp/ContentView.swift
+++ ChecklistApp/ContentView.swift
@@ -9,17 +9,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    var checklistItems = ["Walk the dog", "Brush my teeth", "Learn iOS development", "Soccer practice", "Eat ice cream"]
     var body: some View {
         NavigationView {
             List {
                 Section(header: Text("High Priority")) {
-                    Text("Walk the dog")
-                    Text("Brush my teeth")
-                    Text("Learn iOS development")
+                    Group {
+                        Text(checklistItems[0])
+                        Text(checklistItems[1])
+                        Text(checklistItems[2])
+                        Text(checklistItems[3])
+                        Text(checklistItems[4])
+                    }
+                    Group {
+                        Text("Finish homework")
+                        Text("Change internet provider")
+                        Text("Read Raywenderlich.com")
+                        Text("Clean the kitchen")
+                        Text("Wash the car")
+                    }
                 }
                 Section(header: Text("Low priority")) {
                     Text("Soccer practice")
                     Text("Eat ice cream")
+                    Text("Take vocal lessons")
+                    Text("Record hit single")
+                    Text("Learn every martial art")
+                    Text("Design costume")
+                    Text("Design crime-fighting vehicle")
+                    Text("Come up with superhero name")
+                    Text("Befriend space raccoon")
+                    Text("Save the world")
                 }
             }
             .navigationBarTitle("Checklist")
